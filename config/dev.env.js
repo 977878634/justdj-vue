6edf2e20e9b926e7b08164501e6eb5f49f98b130
justdj <977878634@qq.com>
2019-03-09 09:58:19 +0800

    开发聊天模块 解决服务器webSocket无法连接的问题

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,6 +6,6 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 
   JOB_URL: '"http://127.0.0.1:9002"',
-  // COLLECTION_URL: '"http://127.0.0.1:9003"'
-COLLECTION_URL: '"http://47.102.199.98:9003"'
+  COLLECTION_URL: '"http://127.0.0.1:9003"'
+// COLLECTION_URL: '"http://47.102.199.98:9003"'
 });
--- config/prod.env.js
+++ config/prod.env.js
@@ -3,6 +3,7 @@ module.exports = {
   NODE_ENV: '"production"',
 
   JOB_URL: '"http://122.152.193.63:9002"',
-  COLLECTION_URL: '"http://122.152.193.63:9003"'
+  // COLLECTION_URL: '"http://122.152.193.63:9003"'
+  COLLECTION_URL: '"http://47.102.199.98:9003"'
 
 }
