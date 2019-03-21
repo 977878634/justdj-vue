d7f96a06ab130a5487ddd96da7a423966c83d07b
justdj <977878634@qq.com>
2019-03-21 19:37:33 +0800

    转移collection 服务

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,6 +4,6 @@ module.exports = {
 
   JOB_URL: '"http://122.152.193.63:9006/job"',
   // COLLECTION_URL: '"http://122.152.193.63:9003"'
-  COLLECTION_URL: '"http://122.152.193.63:9003"'
+  COLLECTION_URL: '"http://139.199.88.87:9003"'
 
 };
